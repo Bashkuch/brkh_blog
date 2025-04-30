e72c179ff89ea62bb3efc5816b33a310f4b8982d
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-05-01 01:56:25 +0330

    add author page and not found page

---
+++ app/about/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { coreContent } from 'pliny/utils/contentlayer'
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'About' })
+
+// This function receives params from the dynamic route, and we need to await it
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = await params // Await the params to resolve it
+
+  // Find the author by slug
+  const author = allAuthors.find((author) => author.slug === slug)
+
+  if (!author) {
+    // Handle case when author is not found (e.g., show 404 page)
+    return <div>Author not found</div>
+  }
+
+  // Get content for the author
+  const mainContent = coreContent(author)
+
+  return (
+    <AuthorLayout content={mainContent}>
+      <MDXLayoutRenderer code={author.body.code} />
+    </AuthorLayout>
+  )
+}
