ad00980cc6ff93efd50a26a1d524d5d10cdc59d8
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-05-01 02:04:54 +0330

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'خانه' },
   { href: '/blog', title: 'بلاگ' },
   { href: '/tags', title: 'تگ ها' },
-  { href: '/projects', title: 'پروژه ها' },
+  // { href: '/projects', title: 'پروژه ها' },
   { href: '/about', title: 'درباره ما' },
 ]
 
