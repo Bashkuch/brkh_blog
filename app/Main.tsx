8aa80f1b66e5ee17a66d6efe5ef2a3438695c78a
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-04-30 18:57:20 +0330

    Add HomePage component & add an example blog

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import HomePage from '@/components/HomePage'
 
 const MAX_DISPLAY = 5
 
@@ -11,6 +12,7 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <HomePage />
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             آخرین ها
           </h1>
