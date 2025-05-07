c30aae781e5962dc5ba5076f9461ea2262643bf1
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-05-07 19:19:12 +0330

    Update home page

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,22 +1,28 @@
 const HomePage = () => {
   return (
+    <>
+      <section className="relative min-h-[250px] flex flex-row items-left justify-center text-center text-gray-100 ">
+        <div
+          className="w-full h-[30vh] bg-cover bg-center rounded-md absolute -z-10"
+          style={{ backgroundImage: "url('/static/images/bg.jpg')" }}
+        ></div>
+        <div className="w-full h-[30vh] bg-black/55 absolute rounded-md -z-10"></div>
 
-    <section className="relative min-h-[300px] flex flex-col items-center justify-center text-center px-4 sm:px-6  text-gray-900 dark:text-gray-100">
+        <div className="flex h-[30vh] flex-col justify-center items-center p-8">
+          <h1 className="text-4xl sm:text-3xl md:text-4xl font-extrabold mb-4 sm:mb-6">
+            برکه: بهترین‌ها را در زنجان پیدا کنید
+          </h1>
+          <p className="text-lg sm:text-xl md:text-2xl max-w-4xl mb-6 sm:mb-8">
+            برکه به شما کمک می‌کند بهترین فروشگاه‌ها، رستوران‌ها و خدمات زنجان را پیدا کنید.</p>
+          <button className="px-5 sm:px-6 py-2.5 sm:py-3 bg-primary-600 text-white rounded-xl text-base sm:text-lg hover:bg-primary-700 transition dark:bg-primary-500 dark:hover:bg-primary-600">
+            شروع جستجو
+          </button>
+        </div>
+      </section>
 
-      <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold mb-4 sm:mb-6">
-        برکه
-      </h1>
-      <p className="text-lg sm:text-xl md:text-2xl max-w-4xl mb-6 sm:mb-8">
-        محلی برای معرفی بهترین ها در شهر زنجان
-      </p>
+      <div className="w-full h-40 border border-primary-500 rounded-md my-5"></div>
 
-      <button className="px-5 sm:px-6 py-2.5 sm:py-3 bg-primary-600 text-white rounded-xl text-base sm:text-lg hover:bg-primary-700 transition dark:bg-primary-500 dark:hover:bg-primary-600">
-        شروع جستجو
-      </button>
-      <div className="w-full h-40 bg-primary-500 rounded-md my-20">   </div>
-
-    </section>
-
+    </>
   );
 };
 
